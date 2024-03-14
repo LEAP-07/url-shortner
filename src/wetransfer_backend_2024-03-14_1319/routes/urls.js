6992d6fee2a12ad3b4bffe6c1aba972750f582ca
Mohit <70692495+MohitGupta12@users.noreply.github.com>
2024-03-14 18:56:24 +0530

    Delete src/wetransfer_backend_2024-03-14_1319 directory

--- src/wetransfer_backend_2024-03-14_1319/routes/urls.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const urlController = require('../controller/urlController.js')
-const router = express.Router();
-
-// api call to controller for converting the url
-router.route('/shorten')
-  .post(urlController.convert);
-
-module.exports = router;
